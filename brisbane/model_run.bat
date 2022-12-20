2274cb7566f6f724086affed3c6f31d5a77f3002
Peter Stephan <peter.james.stephan@gmail.com>
2022-12-20 17:11:05 +1000

    ..

---
+++ brisbane/model_run.bat
@@ -0,0 +1,6 @@
+@echo off
+dir="C:\Users\peter\Documents\GitHub\atam-analytics\atam\brisbane"
+
+"C:\Users\peter\Documents\GitHub\Python37\python.exe" "C:\Users\peter\Documents\GitHub\atam-analytics\atam\brisbane\model_run.py"
+
+pause
