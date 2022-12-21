259e507690d59106f3651b5cb88e886d40bf5add
Peter Stephan <peter.james.stephan@gmail.com>
2022-12-21 11:04:18 +1000

    .

--- brisbane/model_run.bat
+++ brisbane/model_run.bat
@@ -1,9 +1,16 @@
 @echo off
+echo ////////////////
+echo Running model...
+echo //////////////// 
 
 :: Set Directories
-set model_dir="C:\Users\peter\Documents\GitHub\atam-analytics\atam\brisbane"
+set model_dir=%cd%
+echo model_dir=%model_dir%
 
 :: Call Model Run
 "%model_dir%\02_scripts\Python37\Python37\python.exe" "%model_dir%\model_run_R0003.py"
 
+echo ////////////////
+echo Run Complete.
+echo //////////////// 
 PAUSE
