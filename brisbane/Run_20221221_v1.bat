c3aed3b6d0a9594063fc1aab97fcc98362593d40
Peter Stephan <peter.james.stephan@gmail.com>
2022-12-21 12:41:53 +1000

    --

--- brisbane/Run_20221221_v1.bat
+++ brisbane/Run_20221221_v1.bat
@@ -11,13 +11,13 @@ echo model_dir=%model_dir%
 set model_id="R0001"
 set network_case="01_BASE_NET"
 set demand_case="01_BASE_DEMAND"
-"%model_dir%\02_scripts\Python37\Python37\python.exe" "%model_dir%\model_run.py" %model_id% %network_case% %demand_case%
+"%model_dir%\02_scripts\Python37\Python37\python.exe" "%model_dir%\02_scripts\model_run.py" %model_id% %network_case% %demand_case%
 
 :: Call Model Run #2
 set model_id="R0002"
 set network_case="02_Example"
 set demand_case="02_Example"
-"%model_dir%\02_scripts\Python37\Python37\python.exe" "%model_dir%\model_run.py" %model_id% %network_case% %demand_case%
+"%model_dir%\02_scripts\Python37\Python37\python.exe" "%model_dir%\02_scripts\model_run.py" %model_id% %network_case% %demand_case%
 
 echo ////////////////
 echo Run Complete.
